b3b88dae7593a7acdc2c6c9fad2094ede19c0e44
Carl-Hugo Marcotte <carlhugo@gmail.com>
2018-12-29 12:09:10 -0500

    Initial script

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # Scripts
+
 Useful scripts
+
+## dotnet-new/dual-projects.ps1
+
+Allows you to create a project of any type (possible with `dotnet new`) and its associated test project (using `xunit`).
+
+The project structure is as follow:
+
+```
+(root)
+src
+  {project name}
+test
+  {project name}.Tests
+(optional){solution name}.sln
+```
+
+### Examples
+
+How to create a project and a test project (named booyaba) in an existing solution:
+
+```powershell
+./dotnet-new/dual-projects.ps1 -projectName booyaba -nosln
+```
+
+How to create a project, a test project (named booyaba) and a solution:
+
+```powershell
+./dotnet-new/dual-projects.ps1 -projectName booyaba
+```
+
+How to create a class library project and a test project (named booyaba) in an existing solution:
+
+```powershell
+./dotnet-new/dual-projects.ps1 -projectName booyaba -nosln -classlib
+OR
+./dotnet-new/dual-projects.ps1 -projectName booyaba -nosln -projectTemplate classlib
+```
+
+How to create a class library project, a test project (named booyaba) and a solution:
+
+```powershell
+./dotnet-new/dual-projects.ps1 -projectName booyaba -classlib
+OR
+./dotnet-new/dual-projects.ps1 -projectName booyaba -projectTemplate classlib
+```
+
+How to create an MVC project and a test project (named booyaba) in an new solution named some-solution.sln in the current directory:
+
+```powershell
+./dotnet-new/dual-projects.ps1 -projectName booyaba -projectTemplate mvc -solutionName some-solution.sln
+```
---
+++ dotnet-new/dual-projects.ps1
@@ -0,0 +1,80 @@
+﻿param (
+    [Parameter(Mandatory = $true)][string]$projectName,
+    [switch]$nosln = $false,
+    [switch]$classlib = $false,
+    [string]$projectTemplate = "web",
+    [string]$solutionName = $null,
+    [switch]$debug = $true
+)
+$createSolution = !$nosln
+$debugColor = "magenta"
+
+if ($debug) {
+    Write-Host "createSolution: $createSolution" -foregroundcolor $debugColor
+    Write-Host "solutionName: $solutionName" -foregroundcolor $debugColor
+    Write-Host "condition1: ", ($createSolution -and $solutionName) -foregroundcolor $debugColor
+    Write-Host "condition2: ", ($createSolution -and !$solutionName) -foregroundcolor $debugColor
+}
+
+if ($createSolution -and $solutionName) {
+    # Create and move to project directory
+    if ($debug) { Write-Host "Creating solution diretory using command: mkdir $projectName" -foregroundcolor $debugColor }
+    mkdir $projectName
+    Set-Location $projectName
+
+    # Create the solution
+    if ($debug) { Write-Host "Creating a new solution" -foregroundcolor $debugColor }
+    dotnet new sln
+}
+elseif ($createSolution -and !$solutionName) {
+    $noExtensionSolutionName = $solutionName -replace ".sln", ""
+    if ($debug) { Write-Host "Creating a solution named $noExtensionSolutionName" -foregroundcolor $debugColor }
+    dotnet new sln -n $noExtensionSolutionName
+}
+
+# Create project paths
+$SrcProjectDir = "src\$projectName"
+$TestProjectDir = "test\$projectName.Tests"
+
+$SrcProjectName = "$projectName"
+$TestProjectName = "$projectName.Tests"
+
+$SrcProjectPath = "$SrcProjectDir\$projectName.csproj"
+$TestProjectPath = "$TestProjectDir\$projectName.Tests.csproj"
+
+# Create project
+if (!(Test-Path src)) {
+    if ($debug) { Write-Host "Creating src diretory using command: mkdir src" -foregroundcolor $debugColor }
+    mkdir src
+}
+Set-Location src
+if ($classlib -and $projectTemplate -eq "web") {
+    $projectTemplate = "classlib";
+}
+if ($debug) { Write-Host "Creating project using command: dotnet new $projectTemplate -n $SrcProjectName" -foregroundcolor $debugColor }
+dotnet new $projectTemplate -n $SrcProjectName
+Set-Location ..
+
+# Create test project
+if (!(Test-Path test)) {
+    if ($debug) { Write-Host "Creating test diretory using command: mkdir test" -foregroundcolor $debugColor }
+    mkdir test
+}
+Set-Location test
+if ($debug) { Write-Host "Creating test project using command: dotnet new xunit -n $TestProjectName" -foregroundcolor $debugColor }
+dotnet new xunit -n $TestProjectName
+Set-Location ..
+
+# Add Reference to test project
+if ($debug) { Write-Host "Adding reference from src to test project using command: dotnet add $TestProjectPath reference $SrcProjectPath" -foregroundcolor $debugColor }
+dotnet add $TestProjectPath reference $SrcProjectPath
+
+# Add projects to solution
+if ($solutionName -eq $null) {
+    if ($debug) { Write-Host "Adding projects to solution using command: dotnet sln add $SrcProjectPath $TestProjectPath" -foregroundcolor $debugColor }
+    dotnet sln add $SrcProjectPath $TestProjectPath
+}
+else {
+    if ($debug) { Write-Host "Adding projects to solution using command: dotnet sln $solutionName add $SrcProjectPath $TestProjectPath" -foregroundcolor $debugColor }
+    dotnet sln $solutionName add $SrcProjectPath $TestProjectPath
+}
